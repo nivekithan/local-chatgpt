abcfbcb95ac771de48d0bf98f6264c834e77ee8a
Nivekithan S <nivekithan363@gmail.com>
2024-04-10 12:46:59 +0530

    use gpt-4 turbo

--- app/lib/openai.ts
+++ app/lib/openai.ts
@@ -42,7 +42,7 @@ export async function getGpt4Result({
       role: message.role,
       content: message.content,
     })),
-    model: "gpt-4",
+    model: "gpt-4-turbo",
     stream: true,
   });
 
